c5f3c56d73416c7e5c035cb7c8ea3ba322692545
Tim La <tlamarketing111@gmail.com>
2022-12-15 07:52:41 -0500

    imported imports for property

---
+++ frontend/components/Property.jsx
@@ -0,0 +1,18 @@
+import Link from 'next/link';
+import Image from 'next/image';
+import { Box, Flex, Text } from '@chakra-ui/layout';
+import { Avatar } from '@chakra-ui/avatar';
+import { FaBed, FaBath } from 'react-icons/fa';
+import { BsGridFill } from 'react-icons/bs';
+import { GoVerified } from 'react-icons/go';
+import millify from 'millify';
+
+import DefaultImage from '../assets/images/house.jpg';
+
+const Property = () => (
+    <Link>
+
+    </Link>
+)
+
+export default Property
