48c508a4f4122aaee93f9736c702dba87f866e7b
Tim La <tlamarketing111@gmail.com>
2022-12-15 07:51:13 -0500

    mapped through properties

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -19,7 +19,6 @@ export const Banner = ({ purpose, title1, title2, desc1, desc2, buttonText, link
 
 
 const Home = ({ propertiesForSale, propertiesForRent }) => {
-  console.log(propertiesForSale, propertiesForRent);
   return (
     <div>
       <h1>Hello World</h1>
@@ -33,6 +32,9 @@ const Home = ({ propertiesForSale, propertiesForRent }) => {
         linkName='/search?purpose=for-rent'
         imageUrl='https://bayut-production.s3.eu-central-1.amazonaws.com/image/145426814/33973352624c48628e41f2ec460faba4'
       />
+      <Flex flexWrap='wrap'>
+        {propertiesForRent.map((property) => <Property property={property} key={property.id} />)}
+      </Flex>
       <Banner
         purpose='BUY A HOME'
         title1=' Find, Buy & Own Your'
@@ -43,6 +45,9 @@ const Home = ({ propertiesForSale, propertiesForRent }) => {
         linkName='/search?purpose=for-sale'
         imageUrl='https://bayut-production.s3.eu-central-1.amazonaws.com/image/110993385/6a070e8e1bae4f7d8c1429bc303d2008'
       />
+      <Flex flexWrap='wrap'>
+        {propertiesForSale.map((property) => <Property property={property} key={property.id} />)}
+      </Flex>
     </div>
   )
 }
