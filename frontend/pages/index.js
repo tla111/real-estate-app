7039d2626f0366ad0294bf5c4fa0009745226951
Tim La <tlamarketing111@gmail.com>
2022-12-15 07:44:46 -0500

    first banner made

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -21,6 +21,16 @@ export default function Home() {
   return (
     <div>
       <h1>Hello World</h1>
+      <Banner
+        purpose='RENT A HOME'
+        title1='Rental Homes for'
+        title2='Everyone'
+        desc1=' Explore from Apartments, builder floors, villas'
+        desc2='and more'
+        buttonText='Explore Renting'
+        linkName='/search?purpose=for-rent'
+        imageUrl='https://bayut-production.s3.eu-central-1.amazonaws.com/image/145426814/33973352624c48628e41f2ec460faba4'
+      />
     </div>
   )
 }
