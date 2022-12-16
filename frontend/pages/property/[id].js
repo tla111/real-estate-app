438fd3fcf6a40cc4e3ed5e94e407fab355e331bd
Tim La <tlamarketing111@gmail.com>
2022-12-16 07:21:50 -0500

    building layout for PropertyDEtails

--- frontend/pages/property/[id].js
+++ frontend/pages/property/[id].js
@@ -6,3 +6,21 @@ import { GoVerified } from 'react-icons/go';
 import millify from 'millify';
 
 import { baseUrl, fetchApi } from '../../utils/fetchApi';
+
+const PropertyDetails = () => (
+    <Box>
+        hi
+    </Box>
+)
+
+export default PropertyDetails;
+
+export async function getServerSideProps({ params: { id } }) {
+    const data = await fetchApi(`${baseUrl}/properties/detail?externalID=${id}`);
+
+    return {
+        props: {
+            propertyDetails: data,
+        },
+    };
+}
