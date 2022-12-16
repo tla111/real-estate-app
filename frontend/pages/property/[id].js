3860bdef9bf9c50ea4bb68ac68782774f7a808f8
Tim La <tlamarketing111@gmail.com>
2022-12-16 07:21:04 -0500

    imported imports for PropertyDetails

---
+++ frontend/pages/property/[id].js
@@ -0,0 +1,8 @@
+import { Box, Flex, Spacer, Text } from '@chakra-ui/layout';
+import { Avatar } from '@chakra-ui/avatar';
+import { FaBed, FaBath } from 'react-icons/fa';
+import { BsGridFill } from 'react-icons/bs';
+import { GoVerified } from 'react-icons/go';
+import millify from 'millify';
+
+import { baseUrl, fetchApi } from '../../utils/fetchApi';
