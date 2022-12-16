79715bef0c469d2d4f3e92a9ebbcc4e2bd8f63b2
Tim La <tlamarketing111@gmail.com>
2022-12-16 07:01:40 -0500

    imported imports for search page

---
+++ frontend/pages/search.js
@@ -0,0 +1,16 @@
+import { useState } from 'react';
+import { useRouter } from 'next/router';
+import Image from 'next/image'
+import { Flex, Box, Text, Icon } from '@chakra-ui/react';
+import { BsFilter } from 'react-icons/bs';
+
+import Property from '../components/Property';
+import SearchFilters from '../components/SearchFilters';
+
+const search = () => {
+    return (
+        <div>search</div>
+    )
+}
+
+export default search
